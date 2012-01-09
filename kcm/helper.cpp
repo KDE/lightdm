71b88df781b7ee633c800d821e72e1bb4692c804
Aurélien Gâteau <aurelien.gateau@canonical.com>
2012-01-09 15:47:22 +0100

    Make sure lightdm-kde-greeter.conf is readable by every users

--- kcm/helper.cpp
+++ kcm/helper.cpp
@@ -4,8 +4,23 @@
 #include <KConfig>
 #include <KConfigGroup>
 
+#include <QFile>
+
 #include "config.h"
 
+static QSharedPointer<KConfig> openConfig()
+{
+    QFile file(LIGHTDM_CONFIG_DIR "/lightdm-kde-greeter.conf");
+    if(!file.exists()) {
+        // If we are creating the config file, ensure it is world-readable: if
+        // we don't do that, KConfig will create a file which is only readable
+        // by root
+        file.open(QIODevice::WriteOnly);
+        file.close();
+        file.setPermissions(QFile::ReadOwner | QFile::WriteOwner | QFile::ReadGroup | QFile::ReadOther);
+    }
+    return QSharedPointer<KConfig>(new KConfig(file.fileName(), KConfig::SimpleConfig));
+}
 
 KAuth::ActionReply Helper::savetheme(const QVariantMap &args)
 {
@@ -13,9 +28,9 @@ KAuth::ActionReply Helper::savetheme(const QVariantMap &args)
 
     kDebug() << "saving theme" << args["theme-name"];
 
-    KConfig config(LIGHTDM_CONFIG_DIR "/lightdm-kde-greeter.conf");
-    config.group("greeter").writeEntry("theme-name", args["theme-name"]);
-    config.sync();
+    QSharedPointer<KConfig> config = openConfig();
+    config->group("greeter").writeEntry("theme-name", args["theme-name"]);
+    config->sync();
 
     //FIXME check result somehow...
 
@@ -25,14 +40,14 @@ KAuth::ActionReply Helper::savetheme(const QVariantMap &args)
 KAuth::ActionReply Helper::savethemedetails(const QVariantMap &args)
 {
     KAuth::ActionReply reply;
-    KConfig config(LIGHTDM_CONFIG_DIR "/lightdm-kde-greeter.conf");
-    KConfigGroup configGroup = config.group("greeter-details");
+    QSharedPointer<KConfig> config = openConfig();
+    KConfigGroup configGroup = config->group("greeter-details");
 
     QMap<QString, QVariant>::const_iterator i;
     for (i = args.constBegin() ; i != args.constEnd() ; i++) {
         configGroup.writeEntry(i.key(), i.value());
     }
-    config.sync();
+    config->sync();
 
     return reply;
 }
