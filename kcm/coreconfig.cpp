94bf1ed11ba36e220029679d0cc8825bc7cc3e39
Aurélien Gâteau <aurelien.gateau@canonical.com>
2012-02-13 19:25:27 +0100

    Wire autologin settings

--- kcm/coreconfig.cpp
+++ kcm/coreconfig.cpp
@@ -11,6 +11,21 @@
 #include <KConfigGroup>
 #include <KDebug>
 
+inline QVariant currentItemData(QComboBox* combo, int role)
+{
+    return combo->itemData(combo->currentIndex(), role);
+}
+
+inline void setCurrentItemFromData(QComboBox* combo, int role, const QVariant& data)
+{
+    int index = combo->findData(data, role);
+    if (index == -1) {
+        kWarning() << "Couldn't find" << data << "(role" << role << ") in combobox" << combo;
+        return;
+    }
+    combo->setCurrentIndex(index);
+}
+
 CoreConfig::CoreConfig(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::CoreConfig)
@@ -23,6 +38,10 @@ CoreConfig::CoreConfig(QWidget *parent) :
     loadFromConfig();
 
     connect(ui->allowGuest, SIGNAL(toggled(bool)), SIGNAL(changed()));
+    connect(ui->autoLogin, SIGNAL(toggled(bool)), SIGNAL(changed()));
+    connect(ui->autoLoginUser, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()));
+    connect(ui->autoLoginSession, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()));
+    connect(ui->autoLoginTimeout, SIGNAL(valueChanged(int)), SIGNAL(changed()));
     connect(ui->enableXdmcp, SIGNAL(toggled(bool)), SIGNAL(changed()));
     connect(ui->enableVnc, SIGNAL(toggled(bool)), SIGNAL(changed()));
 }
@@ -38,6 +57,19 @@ void CoreConfig::loadFromConfig()
 
     ui->allowGuest->setChecked(config.group("SeatDefaults").readEntry("allow-guest", true));
 
+    KConfigGroup lightdmGroup = config.group("LightDM");
+    QString user = lightdmGroup.readEntry("autologin-user");
+    if (!user.isEmpty()) {
+        setCurrentItemFromData(ui->autoLoginUser, QLightDM::UsersModel::NameRole, user);
+    }
+    ui->autoLogin->setChecked(!user.isEmpty());
+
+    QString session = lightdmGroup.readEntry("autologin-session");
+    if (!session.isEmpty()) {
+        setCurrentItemFromData(ui->autoLoginSession, QLightDM::SessionsModel::IdRole, session);
+    }
+    ui->autoLoginTimeout->setValue(lightdmGroup.readEntry("autologin-user-timeout", 0) / 60);
+
     ui->enableXdmcp->setChecked(config.group("XDMCPServer").readEntry("enabled", false));
     ui->enableVnc->setChecked(config.group("VNCServer").readEntry("enabled", false));
 }
@@ -45,6 +77,11 @@ void CoreConfig::loadFromConfig()
 QVariantMap CoreConfig::save()
 {
     QVariantMap args;
+    args["core/LightDM/autologin-user"] = ui->autoLogin->isChecked()
+        ? currentItemData(ui->autoLoginUser, QLightDM::UsersModel::NameRole)
+        : QString();
+    args["core/LightDM/autologin-session"] = currentItemData(ui->autoLoginSession, QLightDM::SessionsModel::IdRole);
+    args["core/LightDM/autologin-user-timeout"] = ui->autoLoginTimeout->value() * 60;
     args["core/SeatDefaults/allow-guest"] = ui->allowGuest->isChecked();
     args["core/XDMCPServer/enabled"] = ui->enableXdmcp->isChecked();
     args["core/VNCServer/enabled"] = ui->enableVnc->isChecked();
