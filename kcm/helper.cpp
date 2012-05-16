910508750e0738bdeafc6b0884fc36522467458c
David Edmundson <kde@davidedmundson.co.uk>
2012-05-16 01:52:04 +0100

    i++ to ++i in for loop

--- kcm/helper.cpp
+++ kcm/helper.cpp
@@ -58,7 +58,7 @@ KAuth::ActionReply Helper::save(const QVariantMap &args)
     QSharedPointer<KConfig> greeterConfig = openConfig(GreeterConfig);
 
     QMap<QString, QVariant>::const_iterator i;
-    for (i = args.constBegin() ; i != args.constEnd() ; i++) {
+    for (i = args.constBegin() ; i != args.constEnd() ; ++i) {
         QStringList lst = i.key().split('/');
         if (lst.size() != 3) {
             errorReply.setErrorDescription(QString("Invalid key format: %1").arg(i.key()));
