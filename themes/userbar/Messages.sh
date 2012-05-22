08ad99aca53517ceb96c9ae20f7021919c8c9973
Albert Astals Cid <tsdgeos@terra.es>
2012-05-22 23:22:45 +0200

    theme.rc is not one of our .rc files that need i18n extraction

--- themes/userbar/Messages.sh
+++ themes/userbar/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$EXTRACTRC `find . -name \*.rc -a ! -name theme.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.qml -o -name \*.cpp` -o $podir/lightdm_theme_userbar.pot
 rm -f rc.cpp
